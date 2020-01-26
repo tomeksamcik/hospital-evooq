9d18ac8dda50dd72b0a9941e8b9831b45a9fb64b
Tomasz Samcik <tomek@samcik.pro>
2020-01-26 14:13:28 +0100

    Description fix

--- src/main/java/ch/evooq/hospital/rules/FlyingSpaghettiMonsterRule.java
+++ src/main/java/ch/evooq/hospital/rules/FlyingSpaghettiMonsterRule.java
@@ -9,7 +9,7 @@ import org.jeasy.rules.annotation.Action;
 import org.jeasy.rules.annotation.Fact;
 import org.jeasy.rules.annotation.Rule;
 
-@Rule(description = "Paracetamol cures Fever")
+@Rule(description = "Spaghetti Monster revives dead man")
 public class FlyingSpaghettiMonsterRule {
 
     public static final int RESURRECTION_CHANCE_ONE_IN = 100_000;
