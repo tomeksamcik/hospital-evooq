2f0b6426972ea09d99f2d8391fdb51097b0498f3
Tomasz Samcik <tomek@samcik.pro>
2020-01-29 18:48:45 +0100

    Added SimpleContextRule

--- src/main/java/ch/evooq/hospital/rules/simple/Rules.java
+++ src/main/java/ch/evooq/hospital/rules/simple/Rules.java
@@ -2,17 +2,15 @@ package ch.evooq.hospital.rules.simple;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
 
+@Builder
 @NoArgsConstructor
 public class Rules {
 
-    @Getter
-    private final List<Rule> rules = new ArrayList<>();
-
-    public Rules add(Rule rule) {
-        rules.add(rule);
-        return this;
-    }
+    @Getter @Singular
+    private List<Rule> rules = new ArrayList<>();
 }
